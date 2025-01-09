4a4d1d3c75609043dcc4d44bce3dae062fb673a3
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2025-01-09 14:20:59 +0000

    commit

---
+++ exJSVariables.js
@@ -0,0 +1,7 @@
+//Introduction to JavaScript Variables
+
+// 1. Using var (older way of declaring variables, avoid in modern code)
+var age = 51; // This is number variable, specifically an Integer
+println("Using var: The age is " + age);
+
+// Change the value of a variable declared with let
